89857aee9d7dfe4119489402e6aa0f5b6734cd47
Diego-PC <diego.palomino@protonmail.com>
2024-04-15 22:09:53 +0200

    Home test design

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SalaComponent } from './pages/sala/sala.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'sala', component: SalaComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
